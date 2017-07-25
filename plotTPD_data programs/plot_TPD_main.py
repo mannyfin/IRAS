9b21c54d6cb4ba59b4db18f1a6c8f1d9bd0e4c4e
manny <scoullos@princeton.edu>
2017-07-25 15:37:16 -0400

    cleanup

--- plotTPD_data programs/plot_TPD_main.py
+++ plotTPD_data programs/plot_TPD_main.py
@@ -11,12 +11,16 @@ import numpy as np
 2. plot uptake of HOAC 
 3. plot uptake of H2 and CO vs uptake of HOAC (combine 1 and 2)
 
+label each curve
+Integrate each curve with separate
+scipy.integrate.trapz(new_file_read,x=new_file_read.index, axis=0)
+
 """
 
 
 # Preliminary stuff
 # Display of temp range on plots. These limits are null if there is more than one column unless otherwise specified
-use_temp_limits = False
+use_temp_limits = True
 low_temp = 100
 high_temp = 600
 single_molecule_name = 'HOAC'
@@ -24,7 +28,21 @@ single_molecule_name = 'HOAC'
 # dotted_lines = [204.7, 161, 395, 428]
 dotted_lines = [204.7, 161, 260, 395, 428]
 
-# dict_values = dict({'HOAC':'61.297', 'CO':})
+
+# dict_values = dict({'HOAC': 61.297,
+#                     'CO': 28.2,
+#                     'H2':1.5,
+#                     'H2O':17.9,
+#                     'CO2': 44.7})
+dict_values = dict({'HOAC': 60.,
+                    'CO': 27.7,
+                    'H2':1.942,
+                    'H2O':17.57,
+                    'CO2': 43.7,
+                    'CH2': 13.6,
+                    '29': 28.8,
+                    'EtOH': 30.7,
+                    'CH4': 14.6})
 
 
 def rename_to_text(file_path):
@@ -38,10 +56,10 @@ def rename_to_text(file_path):
     return file_path, filename
 
 
-def single_slope_subtract(file_read, num_points_to_average_beg=50,num_points_to_average_end=50 ):
+def single_slope_subtract(file__read, num_points_to_average_beg=50,num_points_to_average_end=50 ):
     """
     
-    :param file_read: df of file
+    :param file__read: df of file
     :param num_points_to_average_beg: 
     :param num_points_to_average_end: 
     :return: 
@@ -51,37 +69,62 @@ def single_slope_subtract(file_read, num_points_to_average_beg=50,num_points_to_
     # num_points_to_average_end = 50
 
     # mean of first N points
-    avg_y_beg = file_read.iloc[:num_points_to_average_beg].mean()
+    avg_y_beg = file__read.iloc[:num_points_to_average_beg].mean()
 
     # mean of last N points
-    avg_y_end = file_read.iloc[-num_points_to_average_end:].mean()
+    avg_y_end = file__read.iloc[-num_points_to_average_end:].mean()
 
     # x value for beginning (assume first xval)
-    first_xval = file_read.first_valid_index()
+    first_xval = file__read.first_valid_index()
 
     # x value for ending (assume last xval)
-    last_xval = file_read.last_valid_index()
+    last_xval = file__read.last_valid_index()
 
     slope = (avg_y_end - avg_y_beg) / (last_xval - first_xval)
     # y' = mx
     # caveat...only works with monitoring a single mass-- update 7/13/17 appears to have fixed this...
     # y_prime = pd.DataFrame(slope.values * file_read.index.values, columns=file_read.columns)
 
-    y_prime = pd.DataFrame(np.matmul(file_read.index.values[:, np.newaxis], np.transpose(slope.values[:, np.newaxis])),
-                           columns=file_read.columns)
+    y_prime = pd.DataFrame(np.matmul(file__read.index.values[:, np.newaxis], np.transpose(slope.values[:, np.newaxis])),
+                           columns=file__read.columns)
 
-    y_prime.index = file_read.index
+    y_prime.index = file__read.index
 
     # first attempt at fix
     # y_prime = slope.values[0]*file_read.index+avg_y_beg
 
-    difference = file_read - y_prime
+    difference = file__read - y_prime
     difference = difference - difference.iloc[:(num_points_to_average_beg)].mean()
 
     new_file_read = difference
 
     return new_file_read
 
+
+def plot_same_masses(dict__values, file_name, new__file__read):
+    """
+    Plots the same masses together
+    :param dict__values: dictionary of masses
+    :param file_name: name of the file
+    :param new__file__read: dataframe of the data in the file
+    :return: outputs a plot
+    """
+    i = 1
+    for key, value in dict__values.items():
+
+        try:
+            fig = plt.figure(i, figsize=(15, 7))
+            ax = fig.add_subplot(111)
+            ax.plot(new__file__read.filter(regex=str(value)), label=file_name)
+            plt.ylabel('QMS signal (a.u.)')
+            plt.xlabel('Temperature (K)')
+            plt.title(key + '/Ni(110) TPD')
+            # iterate i to change the figure number for the different mass
+            i += 1
+
+        except ZeroDivisionError:
+            print('ZeroDivisionError: integer division or modulo by zero')
+            print('Mass' + key + 'not found in ' + file_name)
 
 def read_files(file):
     file_path, filename = rename_to_text(file)
@@ -145,83 +188,14 @@ for file in file_path1:
     # new_file_read = single_slope_subtract(new_file_read,num_points_to_average_beg=20,num_points_to_average_end=20)
 
     # PLOTTING
-    # only for 1 column
-    if file_read.columns.__len__() is 1:
-    # plot the data
-    # file_read.plot(x=file_read.index, figsize=(15,7))  # ylabel='QMS signal (a.u.)'
-        ax = plt.figure(123, figsize=(15, 7))
-        plt.plot(new_file_read, label=filename)
 
-    # for multiple columns
-    else:
-        ax = plt.figure(123, figsize=(15, 7))
-        # MASS 61 IS USED SPECIFICALLY FOR HOAC!!!!!!!!
-        try:
-            fig1 = plt.figure(1, figsize=(15, 7))
-            ax1 = fig1.add_subplot(111)
-            # ax1.plot(new_file_read.filter(regex='61'), label=filename)
-            ax1.plot(new_file_read.filter(regex='60'), label=filename)
+    plot_same_masses(dict__values=dict_values,file_name=filename, new__file__read=new_file_read)
 
-            plt.ylabel('QMS signal (a.u.)')
-            plt.xlabel('Temperature (K)')
-            plt.title(single_molecule_name + '/Ni(110) TPD')
-        except ZeroDivisionError:
-            print('ZeroDivisionError: integer division or modulo by zero')
-            print('Mass 61 not found in ' + filename)
-        try:
-            fig2 = plt.figure(2, figsize=(15, 7))
-            ax2 = fig2.add_subplot(111)
-            # ax2.plot(new_file_read.filter(regex='1.5'), label=filename)
-            ax2.plot(new_file_read.filter(regex='1.9'), label=filename)
-
-            plt.ylabel('QMS signal (a.u.)')
-            plt.xlabel('Temperature (K)')
-            plt.title('H2/Ni(110) TPD')
-        except ZeroDivisionError:
-            print('ZeroDivisionError: integer division or modulo by zero')
-            print('Mass 61 not found in ' + filename)
-        try:
-            fig3 = plt.figure(3, figsize=(15, 7))
-            ax3 = fig3.add_subplot(111)
-            # ax3.plot(new_file_read.filter(regex='17.9'), label=filename)
-            ax3.plot(new_file_read.filter(regex='17.5'), label=filename)
-
-            plt.ylabel('QMS signal (a.u.)')
-            plt.xlabel('Temperature (K)')
-            plt.title('H2O/Ni(110) TPD')
-        except ZeroDivisionError:
-            print('ZeroDivisionError: integer division or modulo by zero')
-            print('Mass 18 not found in ' + filename)
-        try:
-            fig4 = plt.figure(4, figsize=(15, 7))
-            ax4 = fig4.add_subplot(111)
-            # ax4.plot(new_file_read.filter(regex='28'), label=filename)
-            ax4.plot(new_file_read.filter(regex='27.7'), label=filename)
-
-            plt.ylabel('QMS signal (a.u.)')
-            plt.xlabel('Temperature (K)')
-            plt.title('CO/Ni(110) TPD')
-        except ZeroDivisionError:
-            print('ZeroDivisionError: integer division or modulo by zero')
-            print('Mass 28 not found in ' + filename)
-        try:
-            fig5 = plt.figure(5, figsize=(15, 7))
-            ax5 = fig5.add_subplot(111)
-            # ax5.plot(new_file_read.filter(regex='44'), label=filename)
-            ax5.plot(new_file_read.filter(regex='43.7'), label=filename)
-
-            plt.ylabel('QMS signal (a.u.)')
-            plt.xlabel('Temperature (K)')
-            plt.title('CO2/Ni(110) TPD')
-        except ZeroDivisionError:
-            print('ZeroDivisionError: integer division or modulo by zero')
-            print('Mass 44 not found in ' + filename)
-        # ax = plt.figure(123, figsize=(15, 7))
-        new_file_read.plot(figsize=(15, 7))
-        # plt.plot(new_file_read)
-        # plt.legend()
+    # new_file_read.plot(figsize=(15, 7))
+    # plt.plot(new_file_read)
+    # plt.legend()
     # set y lower limit = 0
-    axes.Axes.set_ylim(plt.gca(), bottom=0, auto=True)
+    # axes.Axes.set_ylim(plt.gca(), bottom=0, auto=True)
 
     if use_temp_limits is True:
         axes.Axes.set_xlim(plt.gca(), left=low_temp, right=high_temp)
@@ -236,9 +210,6 @@ for file in file_path1:
     else:
         plt.title(single_molecule_name + '/Ni(110) TPD')
     # plt.legend(bbox_to_anchor=(1, 0.5), loc='center', ncol=1)
-
-    # labels = file_read.keys()
-    # plt.get_legend_handles_labels()
 
     # any vertical dotted line values go here
     for x_val in dotted_lines:
